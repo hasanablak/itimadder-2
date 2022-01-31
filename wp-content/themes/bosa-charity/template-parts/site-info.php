ad06de2b59894a6c47709fdab3f2f071bb1d50ff
Hasan Ablak <0hasanablak@gmail.com>
2022-01-31 16:23:11 +0300

    Initial commit

---
+++ wp-content/themes/bosa-charity/template-parts/site-info.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template part for displaying site info
+ *
+ * @package Bosa Charity
+ */
+
+?>
+
+
+<div class="site-info">
+	Copyright © 2022 İtimad Derneği. Tüm hakları saklıdır.
+</div><!-- .site-info -->
---
+++ wp-content/themes/bosa/template-parts/site-info.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template part for displaying site info
+ *
+ * @package Bosa
+ */
+
+?>
+
+<div class="site-info">
+	Copyright © 2022 İtimad Derneği. Tüm hakları saklıdır.
+</div><!-- .site-info -->
